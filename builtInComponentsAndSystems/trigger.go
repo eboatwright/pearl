b2d0b2df5c224bac1aadcda5378cc3c282f990e8
eboatwright <ekb@ranchoburritofarm.com>
2021-08-19 16:08:32 -0500

    19/8/21 4:08 PM

--- builtInComponentsAndSystems/trigger.go
+++ builtInComponentsAndSystems/trigger.go
@@ -22,6 +22,7 @@ func (ts *TriggerSystem) Update(entity *pearl.Entity, scene *pearl.Scene) {
 	otherColliders := scene.FindEntitiesOfType("boxCollider")
 	trigger := entity.GetComponent("trigger").(*Trigger)
 	for _, other := range otherColliders {
+		if other == entity { continue }
 		if EntitiesOverlap(entity, other) {
 			trigger.OnTrigger(entity, other)
 		}
@@ -30,6 +31,6 @@ func (ts *TriggerSystem) Update(entity *pearl.Entity, scene *pearl.Scene) {
 
 func (ts *TriggerSystem) Draw(entity *pearl.Entity, scene *pearl.Scene, screen *ebiten.Image, options *ebiten.DrawImageOptions) {}
 
-func (ts *TriggerSystem) GetRequirement() []string {
+func (ts *TriggerSystem) GetRequirements() []string {
 	return []string { "boxCollider", "trigger" }
 }
