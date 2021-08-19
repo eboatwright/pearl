75e11ffb0688859a3f1b9a346b940868c1b432a3
eboatwright <ekb@ranchoburritofarm.com>
2021-08-19 15:58:41 -0500

    19/8/21 3:58

---
+++ builtInComponentsAndSystems/trigger.go
@@ -0,0 +1,35 @@
+package builtInComponentsAndSystems
+
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/eboatwright/pearl"
+)
+
+
+type onTriggerFunction func(other *pearl.Entity)
+
+
+type Trigger struct {
+	onTrigger onTriggerFunction
+}
+func (t *Trigger) ID() string { return "trigger" }
+
+
+type TriggerSystem struct {}
+
+func (ts *TriggerSystem) Update(entity *pearl.Entity, scene *pearl.Scene) {
+	otherColliders := scene.FindEntitiesOfType("boxCollider")
+	trigger := entity.GetComponent("trigger").(*Trigger)
+	for _, other := range otherColliders {
+		if EntitesOverlap(entity, other) {
+			trigger.onTrigger()
+		}
+	}
+}
+
+func (ts *TriggerSystem) Draw(entity *pearl.Entity, scene *pearl.Scene, screen *ebiten.Image, options *ebiten.DrawImageOptions) {}
+
+func (ts *TriggerSystem) GetRequirement() []string {
+	return []string { "boxCollider", "trigger" }
+}
